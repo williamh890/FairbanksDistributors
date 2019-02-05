b8c6f0e16a44eadefecf1c5f76a0810dee382bce
Hal D <hadimarchi@alaska.edu>
2019-02-04 16:09:38 -0900

    zappafied and dummy csv file attached to emails

--- backend/app/order.py
+++ backend/app/order.py
@@ -1,7 +1,11 @@
 import configparser
 from flask import Blueprint, request, url_for, redirect
 import smtplib
+from email import encoders
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+import mimetypes
 
 order = Blueprint('order', __name__)
 
@@ -13,16 +17,21 @@ def order_success():
 def place_order():
     if request.method == 'POST':
         print(request.form)
-        send_order(request.form)
+        write_order_csv(request.form)
+        send_order()
     else:
-        send_order("testing")
+        write_order_csv("testing")
+        send_order()
     return redirect(url_for('order.order_success'))
 
+def write_order_csv(order_info):
+    with open("order.csv", "w") as order:
+        order.write(order_info)
 
-def send_order(order_info):
+def send_order():
     smtp_config = get_smtp_config()
 
-    email = create_email(order_info, smtp_config)
+    email = create_email(smtp_config)
 
     with smtplib.SMTP_SSL(smtp_config['SERVER']['smtpServerURL'],
                          smtp_config['SERVER']['smtpServerPort']) as server:
@@ -35,10 +44,25 @@ def get_smtp_config():
     smtp_config.read('app/email_creds.cfg')
     return smtp_config
 
-def create_email(order_info, smtp_config):
-    email = MIMEText(order_info)
+def create_email(smtp_config):
+    email = MIMEMultipart()
     email['Subject'] = 'An order'
     email['From'] = smtp_config['USER']['smtpUserAddress']
     email['To'] = smtp_config['DEST']['destAddress']
+
+    ctype, encoding = mimetypes.guess_type("order.csv")
+    if ctype is None or encoding is not None:
+        print("henlo")
+        ctype = "application/octet-stream"
+
+    maintype, subtype = ctype.split("/", 1)
+    attachment = MIMEBase(maintype, subtype)
+
+    with open("order.csv", "rb") as order:
+        attachment.set_payload(order.read())
+
+    encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", "attachment", filename="order.csv")
+    email.attach(attachment)
 
     return email
