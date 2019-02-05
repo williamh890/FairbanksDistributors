ab31751782ad39616e9615807b5815d66a77f002
William Horn <wbhorn@alaska.edu>
2019-02-05 03:17:45 -0900

    Start on data store, start formatting order data, selectable chip types

--- order-app/src/main.js
+++ order-app/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vue-rx'
+import './plugins/vuex'
 import App from './App.vue'
+
+import store from './store';
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
