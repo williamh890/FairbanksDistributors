8f550dad42b34854af26a864edfc21d72a9ec5e5
William Horn <wbhorn@alaska.edu>
2019-02-05 12:38:51 -0900

    Allow item to be set to zero

--- order-app/src/components/Order.vue
+++ order-app/src/components/Order.vue
@@ -49,7 +49,7 @@
 export default {
   name: 'Order',
   data: () => ({
-    numbers: [1,2,3,4,5,6,7,8,9,10],
+    numbers: [0,1,2,3,4,5,6,7,8,9,10],
     type: null,
     chips: {
       'Doritors': [{
