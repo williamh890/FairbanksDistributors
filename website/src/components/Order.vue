34920029605df346d518526ba2d3afc9416c472f
DuaneShaffer <dlshaffer@alaska.edu>
2019-02-13 22:04:43 -0900

    Feat: Dummy order settings page

--- website/src/components/Order.vue
+++ website/src/components/Order.vue
@@ -1,35 +1,47 @@
 <template>
   <v-container class="size" fluid grid-list-xl>
     <v-stepper v-model="element">
+
       <v-stepper-header>
-        <v-stepper-step :complete="element > 1" step="1">Create</v-stepper-step>
-
+        <v-stepper-step :complete="element > 1" step="1">Settings</v-stepper-step>
         <v-divider></v-divider>
-
-        <v-stepper-step :complete="element > 2" step="2">Review</v-stepper-step>
-
+        <v-stepper-step :complete="element > 2" step="2">Create</v-stepper-step>
         <v-divider></v-divider>
-
-        <v-stepper-step :complete="element > 3" step="3">Success</v-stepper-step>
+        <v-stepper-step :complete="element > 3" step="3">Review</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete ="element > 4" step="4">Success</v-stepper-step>
       </v-stepper-header>
 
       <v-stepper-items>
         <v-stepper-content step="1">
+          <OrderSettings />
+
+            <v-btn
+              color="primary"
+              @click="element =2"
+              >
+              Continue
+            </v-btn>
+        </v-stepper-content>
+        <v-stepper-content step="2">
           <OrderCreate />
 
             <v-btn
               block
               color="primary"
               style="margin-top: 10px"
-              v-on:click="element = 2"
+              v-on:click="element = 3"
             >
               Review Order
             </v-btn>
+          <v-btn flat
+            @click="element = 1"
+            >Back</v-btn>
 
 
         </v-stepper-content>
 
-        <v-stepper-content step="2">
+        <v-stepper-content step="3">
           <OrderReview />
 
           <v-btn
@@ -42,12 +54,12 @@
           </v-btn>
 
           <v-btn flat
-            @click="element = 1"
+            @click="element = 2"
             >Edit</v-btn>
 
             <v-progress-circular v-if="isLoading" indeterminate color="secondary" ></v-progress-circular>
       </v-stepper-content>
-      <v-stepper-content step="3">
+      <v-stepper-content step="4">
         <OrderSuccess />
 
         <div v-if="resp">
@@ -61,6 +73,7 @@
 </template>
 
 <script>
+import OrderSettings from './OrderSettings';
 import OrderCreate from './OrderCreate';
 import OrderReview from './OrderReview';
 import OrderSuccess from './OrderSuccess';
@@ -70,6 +83,7 @@ export default {
   name: 'Order',
   store,
   components: {
+    OrderSettings,
     OrderCreate,
     OrderReview,
     OrderSuccess,
@@ -83,7 +97,9 @@ export default {
     return {
       element: 0,
       isLoading: false,
-      resp: null
+      resp: null,
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
     };
   },
   methods: {
@@ -99,7 +115,7 @@ export default {
       this.$http.get('https://backend.uafhalpost.net/place_order')
         .then(
           resp => {
-            this.element = 3;
+            this.element = 4;
             this.isLoading = false;
             this.resp = resp;
           }
---
+++ website/src/components/OrderSettings.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <h1> Order Settings </h1>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="true"
+          :nudge-right="40"
+          :return-value.sync="date"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+            <v-date-picker v-model="date" no-title scrollable>
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+            </v-date-picker>
+        </v-menu>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "OrderSettings"
+    }
+</script>
+
+<style scoped>
+
+</style>
