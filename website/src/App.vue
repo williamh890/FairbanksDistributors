ed90341ee7ffaea0ef09fb7c3dd3b58f1916d269
DuaneShaffer <dlshaffer@alaska.edu>
2019-02-23 11:04:23 -0900

    Fix: Add the rest of the required text..

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-toolbar app
-    v-if="logged">
+    v-if="isLoggedIn">
       <v-toolbar-items>
         <v-img
             :src="require('../../images/FD_Monogram.png')"
