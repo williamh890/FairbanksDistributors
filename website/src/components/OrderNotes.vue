49775331c8e9a74b8e879670c0ee434d1b322dac
DuaneShaffer <dlshaffer@alaska.edu>
2019-03-07 20:02:43 -0900

    Add text area

--- website/src/components/OrderNotes.vue
+++ website/src/components/OrderNotes.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h1>Order Notes</h1>
+      <v-textarea v-model="orderNotes">Give me input</v-textarea>
   </v-container>
 </template>
 <script>
@@ -9,6 +10,9 @@ import store from '../store';
 export default {
   name: 'Notes',
   store,
+    data: () => ({
+    orderNotes: '',
+  })
 }
 
 </script>
