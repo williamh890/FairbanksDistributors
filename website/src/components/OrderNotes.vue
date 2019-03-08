ef72911748684c2b6ccbb6ec7e87ee9de0a5b3b6
DuaneShaffer <dlshaffer@alaska.edu>
2019-03-07 19:54:37 -0900

    Create order notes skeleton

---
+++ website/src/components/OrderNotes.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1>Order Notes</h1>
+  </v-container>
+</template>
+<script>
+import store from '../store';
+
+export default {
+  name: 'Notes',
+  store,
+}
+
+</script>
+
+<style>
+</style>
