74ba0ec781a3aea473e5176ae9d130b46d278702
William Horn <wbhorn@alaska.edu>
2019-03-21 16:58:32 -0800

    excelParse lambda uploads json to s3 bucket

--- excelParse/chip_form.py
+++ excelParse/chip_form.py
@@ -3,9 +3,6 @@ import csv
 import math
 import json
 
-import xlrd
-import csv
-
 
 def csv_from_excel():
     wb = xlrd.open_workbook('/tmp/chipForm.xls')
@@ -19,15 +16,16 @@ def csv_from_excel():
     your_csv_file.close()
 
 
-def get_data():
-    book = xlrd.open_workbook('/tmp/chipForm.xls')
+def get_data(xls_path):
+    book = xlrd.open_workbook(xls_path)
     sheet = book.sheet_by_index(0)
     num_pages = math.ceil(sheet.nrows // 70)
     return_data = []
     for page in range(num_pages):
         row_range = range(page*70, min(page*70+70, sheet.nrows))
         return_data += [[sheet.cell_value(r, c) for c in range(0, 5)] for r in row_range] + \
-                       [[sheet.cell_value(r, c) for c in range(5, 10)] for r in row_range]
+                       [[sheet.cell_value(r, c) for c in range(5, 10)]
+                        for r in row_range]
     return return_data
 
 
@@ -45,17 +43,27 @@ def get_items(data):
     return [row[1] for row in data if not is_category(row) and row[1] != '']
 
 
-def make_json(data):
-    all_items, current_category = {category: [] for category in get_categories(data)}, None
+def get_chips_by_category(data):
+    all_items, current_category = {category: []
+                                   for category in get_categories(data)}, None
     for row in data:
         if is_category(row):
             current_category = row[1]
         elif row[1] != '':
-            all_items[current_category].append(row[1:])
-    return json.dumps(all_items)
+            name, oz, upc, case = row[1:]
+
+            item = {
+                'name': name,
+                'oz': oz,
+                'upc': upc,
+                'case': case
+            }
+
+            all_items[current_category].append(item)
+
+    return all_items
 
 
 if __name__ == "__main__":
     csv_from_excel()
     data = get_data()
-
