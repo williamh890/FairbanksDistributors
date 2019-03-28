99aa7fefd6801279ee988e6d0bbc13f1c76eb733
jacob-butler <jacobbutler858@gmail.com>
2019-03-28 02:24:09 -0800

    Updated backend to create orders as xlsx and update email script to pull xlsx from email

--- email_scripts/windows_scrape_inbox_and_print.py
+++ email_scripts/windows_scrape_inbox_and_print.py
@@ -22,9 +22,9 @@ def get_server():
 
 def extract_and_write_attachment(msg):
     attachment = msg.get_payload()[0]
-    if attachment.get_content_type() == 'text/csv':
+    if attachment.get_content_type() == 'text/xlsx':
         open('order.csv', 'wb').write(attachment.get_payload(decode=True))
-        os.startfile(".\{}".format('order.csv'), "print")
+        os.startfile(".\{}".format('order.xlsx'), "print")
 
 def scrape_inbox():
     server = get_server()
