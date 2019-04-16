fc1f6b46581cc80bc5c30f20cae1e8db62612052
William Horn <wbhorn@alaska.edu>
2019-04-16 10:39:09 -0800

    Add case total to review screen

--- website/src/components/OrderReview.vue
+++ website/src/components/OrderReview.vue
@@ -15,17 +15,29 @@
             </v-list-tile-title>
             <v-list-tile-sub-title>
               <b>upc:</b> {{ item.upc }}, <b>oz:</b> {{ item.oz }}, <b>case:</b> {{ item.case }}
-
             </v-list-tile-sub-title>
           </v-list-tile-content>
 
         <v-list-tile-action>
-           <v-chip color="secondary" text-color="white">
+           <v-chip color="primary" text-color="white">
              {{ item.amount }}
            </v-chip>
         </v-list-tile-action>
         </v-list-tile>
       </template>
+
+      <v-divider></v-divider>
+      <v-spacer></v-spacer>
+
+      <v-list-tile>
+        <v-list-tile-content>
+          <b>Total Cases:</b>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <b>{{ totalCases(orderItems) }}</b>
+        </v-list-tile-action>
+      </v-list-tile>
     </v-list>
 
   </v-container>
@@ -39,6 +51,13 @@ export default {
   computed: {
     orderItems() {
       return this.$store.getters.getOrderItems;
+    }
+  },
+  methods:  {
+    totalCases: function(orderItems) {
+      return orderItems
+        .map(item => item.amount)
+        .reduce((a1, a2) => a1 + a2, 0);
     }
   }
 }
