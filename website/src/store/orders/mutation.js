a3148f748e144681939eee003c44af53f4203761
DuaneShaffer <dlshaffer@alaska.edu>
2019-04-16 17:16:15 -0800

    Fix: Clear order notes when a new order is started

--- website/src/store/orders/mutation.js
+++ website/src/store/orders/mutation.js
@@ -78,6 +78,7 @@ export const mutations = {
 
   [CLEAR_ORDER_SETTINGS]: function(state) {
     state.order.deliveryLocation = null;
+    state.order.orderNotes = '';
   },
 
   [SUBMIT_ORDER]: function(state) {
