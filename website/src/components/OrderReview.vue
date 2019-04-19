5e10ce3dc7fcfde59bf75317338dbd87b968aa35
DuaneShaffer <dlshaffer@alaska.edu>
2019-04-18 21:16:45 -0800

    Feat: Make subheaders bigger on order review screen

--- website/src/components/OrderReview.vue
+++ website/src/components/OrderReview.vue
@@ -4,7 +4,7 @@
 
     <v-list two-line>
       <template v-for="category in orderItems">
-      <v-subheader> {{ category.name }} </v-subheader>
+      <v-subheader class="headline"> {{ category.name }} </v-subheader>
       <template v-for="item in category.items">
         <v-list-tile
           :key="item.name"
