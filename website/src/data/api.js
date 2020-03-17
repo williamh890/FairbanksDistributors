79cf5163294f3a35092aa7dbb0539b41258d7c6e
William Horn <wbhorn@alaska.edu>
2020-03-16 18:52:44 -0800

    Set api url back to dev deployment

--- website/src/data/api.js
+++ website/src/data/api.js
@@ -1,2 +1,1 @@
-//export const apiUrl = 'https://bcpyv36lol.execute-api.us-west-2.amazonaws.com/dev';
-export const apiUrl = 'http://127.0.0.1:5000';
+export const apiUrl = 'https://bcpyv36lol.execute-api.us-west-2.amazonaws.com/dev';
